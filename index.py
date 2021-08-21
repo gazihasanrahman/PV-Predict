2b3c5fab9742f72fbb70cb482c8ebb9f51a7958a
Gazi Hasan Rahman <gazi.hasan.rahman@gmail.com>
2021-08-21 21:44:43 +0100

    Beta V2

--- index.py
+++ index.py
@@ -36,7 +36,6 @@ uploaded_file = st.sidebar.file_uploader("Upload the input CSV file", type=["csv
 
 
 # load the input dataset
-# input_dataset = pd.read_csv('input_1.csv')
 input_dataset = pd.read_csv(uploaded_file)
 
 
@@ -46,63 +45,6 @@ st.sidebar.header('Set the sunrise and sunset')
 st.cache()
 sunrise = st.sidebar.text_input('Sunrise', max_chars=5, value='05:40', help='Example: 05:40')
 sunset = st.sidebar.text_input('Sunset', max_chars=5, value='21:00', help='Example: 21:00')
-
-
-
-
-
-
-
-# if uploaded_file is not None:
-#     input_dataset = pd.read_csv(uploaded_file)
-# else:
-#     def user_input_features():
-#         island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-#         sex = st.sidebar.selectbox('Sex',('male','female'))
-#         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-#         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-#         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-#         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-#         data = {'island': island,
-#                 'bill_length_mm': bill_length_mm,
-#                 'bill_depth_mm': bill_depth_mm,
-#                 'flipper_length_mm': flipper_length_mm,
-#                 'body_mass_g': body_mass_g,
-#                 'sex': sex}
-#         features = pd.DataFrame(data, index=[0])
-#         return features
-#     input_dataset = user_input_features()
-
-# # Combines user input features with entire penguins dataset
-# # This will be useful for the encoding phase
-# penguins_raw = pd.read_csv('penguins_cleaned.csv')
-# penguins = penguins_raw.drop(columns=['species'])
-# df = pd.concat([input_dataset,penguins],axis=0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -184,35 +126,6 @@ predict_weather_50 = model_weather_50.predict(input_weather_dataset)
 predict_weather_55 = model_weather_55.predict(input_weather_dataset)
 predict_weather_60 = model_weather_60.predict(input_weather_dataset)
 
-
-
-
-
-# if uploaded_file is not None:
-#     input_dataset = pd.read_csv(uploaded_file)
-# else:
-#     def user_input_features():
-#         island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-#         sex = st.sidebar.selectbox('Sex',('male','female'))
-#         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-#         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-#         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-#         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-#         data = {'island': island,
-#                 'bill_length_mm': bill_length_mm,
-#                 'bill_depth_mm': bill_depth_mm,
-#                 'flipper_length_mm': flipper_length_mm,
-#                 'body_mass_g': body_mass_g,
-#                 'sex': sex}
-#         features = pd.DataFrame(data, index=[0])
-#         return features
-#     input_dataset = user_input_features()
-
-# # Combines user input features with entire penguins dataset
-# # This will be useful for the encoding phase
-# penguins_raw = pd.read_csv('penguins_cleaned.csv')
-# penguins = penguins_raw.drop(columns=['species'])
-# df = pd.concat([input_dataset,penguins],axis=0)
 
 
 # to make sure there is no negative value in prediction
@@ -575,11 +488,6 @@ predicted_data['time'] = predicted_data['time'].dt.strftime('%H:%M')
 
 
 
-
-predicted_data.to_csv('predicted_data.csv')
-
-
-
 prediction_chart = pd.DataFrame([
     [current_time + pd.Timedelta(minutes=5), predict_full_05.round(1).item(), predict_weather_05.round(1).item()],
     [current_time + pd.Timedelta(minutes=10), predict_full_10.round(1).item(), predict_weather_10.round(1).item()],
@@ -604,115 +512,9 @@ prediction_chart = prediction_chart.set_index('Time')
 
 
 
-# input_dataset = input_dataset.sort_values(by=['Timestamp'], ascending=True)
-
-
-
-# # set dawn and dusk
-# dawn = 285
-# dusk = 1120
-
-# # copy the input data to X_test and pv_prediction
-# X_test = input_dataset.copy()
-# pv_prediction = input_dataset.copy()
-
-# # separate the Month, Day and Time
-# X_test['Month'] = pd.DatetimeIndex(X_test['Timestamp']).month
-# X_test['Day'] = pd.DatetimeIndex(X_test['Timestamp']).day
-# X_test['Time'] = X_test['Timestamp'].dt.hour*60 + X_test['Timestamp'].dt.minute
-
-# # keep only last hour's record
-# X_test = X_test.sort_values(by=['Timestamp'], ascending=True)
-# X_test = X_test.head(12)
-# start_time = X_test['Timestamp'].iloc[0]
-# end_time = X_test['Timestamp'].iloc[-1]
-
-# # identify for which time prediction is going to be made
-# M5 = end_time + pd.Timedelta(minutes=5)
-# M10 = end_time + pd.Timedelta(minutes=10)
-# M15 = end_time + pd.Timedelta(minutes=15)
-# M20 = end_time + pd.Timedelta(minutes=20)
-# M25 = end_time + pd.Timedelta(minutes=25)
-# M30 = end_time + pd.Timedelta(minutes=30)
-# M35 = end_time + pd.Timedelta(minutes=35)
-# M40 = end_time + pd.Timedelta(minutes=40)
-# M45 = end_time + pd.Timedelta(minutes=45)
-# M50 = end_time + pd.Timedelta(minutes=50)
-# M55 = end_time + pd.Timedelta(minutes=55)
-# M60 = end_time + pd.Timedelta(minutes=60)
-
-# # reset index
-# X_test = X_test.reset_index()
-# X_test = X_test.drop(['index'], axis=1)
-
-# X_test = X_test.transpose()
-
-# X_test = X_test.drop(X_test.index[[0,2,3,4]])
-# X_test.reset_index(drop=True, inplace=True)
-
-# #add time to the dataframe
-# X_test['Timestamp'] = M5
-# X_test['Month'] = pd.DatetimeIndex(X_test['Timestamp']).month
-# X_test['Day'] = pd.DatetimeIndex(X_test['Timestamp']).day
-# X_test['Time'] = X_test['Timestamp'].dt.hour*60 + X_test['Timestamp'].dt.minute
-# X_test = X_test.drop(['Timestamp'], axis=1)
-
-# # rearrange and change column name
-# cols = X_test.columns.tolist()
-# cols = cols[-3:] + cols[:-3]
-# X_test = X_test[cols]
-# X_test.columns = ['Month', 'Day', 'Time', 't-60', 't-55', 't-50', 't-45', 't-40', 't-35', 't-30', 't-25', 't-20', 't-15', 't-10', 't-5']
-
-# # import the scaler
-# scaling = pickle.load(open('scaler.pkl', 'rb'))
-
-# # scale the imported data using standard scaler
-# X_test = scaling.transform(X_test)
-
-# # import the ml model
-# model = pickle.load(open('ml_model.pkl', 'rb'))
-
-# # make the prediction
-# y_pred = model.predict(X_test)
-
-# # to make sure there is no negative value in prediction
-# if y_pred < 0:
-#     y_pred = 0
-# else:
-#     y_pred = y_pred
-
-# # to make sure no power output before dawn or after dusk
-# if (M5.hour*60 + M5.minute) < dawn:
-#     y_pred = 0
-# elif (M5.hour*60 + M5.minute) > dusk:
-#     y_pred = 0
-# else:
-#     y_pred = y_pred
-
-# # add the prodiction to the input data
-# pv_prediction.loc[-1] = [M5, y_pred.round(1).item()]
-# pv_prediction.reset_index(drop=True, inplace=True)
-
-# pv_prediction.to_csv('pv_prediction.csv', index=None)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # altair chart
 
 source2 = predicted_data.copy()
-# source2 = pd.read_csv("pv_prediction2.csv")
 
 
 nearest = alt.selection_single(nearest=True, on='mouseover', fields=['time'], empty='none', clear="mouseout")
@@ -733,8 +535,6 @@ text = line.mark_text(align='left', dx=5, dy=-5).encode(text=alt.condition(neare
 
 rules = alt.Chart(source2).mark_rule(color='gray').encode(x ='time').transform_filter(nearest)
 
-# alt.layer(line, selectors, points, rules, text).properties(width=800, height=400).configure_axis(grid=True)
-
 predict_chart = alt.layer(line, selectors, points, rules, text).properties(width=1200, height=400).configure_axis(grid=True)
 
 st.write(predict_chart)
@@ -744,12 +544,11 @@ st.write(predict_chart)
 
 st.subheader('**Table:** PV power generation prediction for the next 1 hour in five-minute intervals')
 
-# st.table(data=prediction_chart.round(2))
 
 st.table(prediction_chart.style.format({"Predicted PV Power Output": "{:.2f}", "Alternative Prediction by Weather": "{:.2f}"},))
 
-
-url = 'file:///C:/Users/hasan/OneDrive%20-%20University%20of%20East%20Anglia/Dissertation%20(7027X)/PV%20power%20using%20ML/webapp/Prediction.csv'
+url = 'https://raw.githubusercontent.com/gazihasanrahman/Predicting-PV-power-generation-using-ML/Prediction.csv'
+# url = 'file:///C:/Users/hasan/OneDrive%20-%20University%20of%20East%20Anglia/Dissertation%20(7027X)/PV%20power%20using%20ML/webapp/Prediction.csv'
 
 if st.sidebar.button('Export your result'):
     webbrowser.open_new_tab(url)
